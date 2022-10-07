41e2caccc51e1d5697b1eb8e37995c988de58701
jjtaguba <tagubahidden@gmail.com>
2022-10-07 08:02:27 -0400

    Corrects description of non qualifying interest deposit integration test.

--- src/test/java/net/testudobank/tests/MvcControllerIntegTest.java
+++ src/test/java/net/testudobank/tests/MvcControllerIntegTest.java
@@ -1678,7 +1678,7 @@ public void testTransferPaysOverdraftAndDepositsRemainder() throws SQLException,
   }
 
   /**
-   * Verifies interest upon 5th deposit.
+   * Verifies no interest upon 5th deposit with 4 qualifying deposits and 1 non qualifying deposit.
    * The customer's Balance in the Customers table should be increased,
    * and the Deposit should be logged in the TransactionHistory table.
    * 
