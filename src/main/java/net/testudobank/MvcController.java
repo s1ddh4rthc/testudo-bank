10b42b17049b0107d2ea5ac78c1af26d4dd78362
Benjamin Jean <benjamin2jean@yahoo.com>
2023-02-10 22:05:59 -0500

    added interest helper method to de-clutter controller logic

--- src/main/java/net/testudobank/MvcController.java
+++ src/main/java/net/testudobank/MvcController.java
@@ -362,6 +362,12 @@ public class MvcController {
     updateAccountInfo(user);
     return "account_info";
   }
+  
+  //Helper method by benjamin
+  //Done for code reusability
+  public int applyInterestRateToPennyAmount(int pennyAmount){
+    return (int) (pennyAmount * INTEREST_RATE);
+  }
 	
   /**
    * HTML POST request handler for the Withdraw Form page.
@@ -407,7 +413,7 @@ public class MvcController {
     int userWithdrawAmtInPennies = convertDollarsToPennies(userWithdrawAmt); // dollar amounts stored as pennies to avoid floating point errors
     String currentTime = SQL_DATETIME_FORMATTER.format(new java.util.Date()); // use same timestamp for all logs created by this deposit
     int userBalanceInPennies = TestudoBankRepository.getCustomerCashBalanceInPennies(jdbcTemplate, userID);
-    int userOverdraftBalanceInPennies = TestudoBankRepository.getCustomerOverdraftBalanceInPennies(jdbcTemplate, userID);
+    int userOverdraftBalanceInPennies = applyInterestRateToPennyAmount(userBalanceInPennies);
     if (userWithdrawAmtInPennies > userBalanceInPennies) { // if withdraw amount exceeds main balance, withdraw into overdraft with interest fee
       int excessWithdrawAmtInPennies = userWithdrawAmtInPennies - userBalanceInPennies;
       int newOverdraftIncreaseAmtAfterInterestInPennies = (int)(excessWithdrawAmtInPennies * INTEREST_RATE);
