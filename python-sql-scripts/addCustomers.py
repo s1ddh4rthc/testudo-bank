a8e258436da8cb36d327054fa092cf5f9c9a572b
Valine-Uzoukwu <Vuzoukwu@terpmail.umd.edu>
2023-11-29 00:09:17 -0500

    Changed create.db to initilize all customer savings and numWithdrawls to 0

--- python-sql-scripts/addCustomers.py
+++ python-sql-scripts/addCustomers.py
@@ -135,6 +135,7 @@ for i in range(num_customers_to_add):
     customer_last_name = names.get_last_name()
     customer_balance = random.randint(100, 10000) * 100 # multiply by 100 to have a penny value of 0
     customer_password = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k = 9))
+  
     
     # add random customer ID, name, and balance to Customers table.
     # all customers start with Overdraft balance of 0
@@ -151,6 +152,19 @@ for i in range(num_customers_to_add):
                 0,
                 0)
     cursor.execute(insert_customer_sql)
+    
+    # add random customer ID,  and name, SavingsAccounts table.
+    # all customers start with Savings balance of 0
+    # all customers start with a numWithdrawls of 0
+    insert_savingsaccounts_sql = '''
+    INSERT INTO Savingsaccounts
+    VALUES  ({0},{1},{2},{3},{4});
+    '''.format("'" + customer_id + "'",
+                "'" + customer_first_name + "'",
+                "'" + customer_last_name + "'",
+                0,
+                0)
+    cursor.execute(insert_savingsaccounts_sql) 
     
     # add customer ID and password to Passwords table
     insert_password_sql = '''
