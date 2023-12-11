5be73296f3208eacb8df452f085c63a010c64358
vedasingireddy <vedasingireddy@gmail.com>
2023-12-11 04:23:53 -0500

    Add integration test for basic case

--- src/test/java/net/testudobank/tests/MvcControllerIntegTest.java
+++ src/test/java/net/testudobank/tests/MvcControllerIntegTest.java
@@ -2,6 +2,7 @@ package net.testudobank.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.description;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -1580,6 +1581,69 @@ public void testTransferPaysOverdraftAndDepositsRemainder() throws SQLException,
             .shouldSucceed(false)
             .build();
     cryptoTransactionTester.test(cryptoTransaction);
+  }
+
+  /**
+   * Verifies the simplest set of deposits that trigger an applied interest.
+   * The customer's Balance in the Customers table should be increased,
+   * and the Applied Interest Deposit should be logged in the TransactionHistory table.
+   * 
+   * Assumes that the customer's account is in the simplest state
+   * (not in overdraft, account is not frozen due to too many transaction disputes, etc.)
+   * 
+   * @throws SQLException
+   * @throws ScriptException
+   */
+  @Test
+  public void testSimpleAppliedInterestDeposit() throws SQLException, ScriptException {
+    // initialize customer1 with a balance of $123.45 (to make sure this works for non-whole dollar amounts). represented as pennies in the DB.
+    double CUSTOMER1_BALANCE = 123.45;
+    int CUSTOMER1_BALANCE_IN_PENNIES = MvcControllerIntegTestHelpers.convertDollarsToPennies(CUSTOMER1_BALANCE);
+    MvcControllerIntegTestHelpers.addCustomerToDB(dbDelegate, CUSTOMER1_ID, CUSTOMER1_PASSWORD, CUSTOMER1_FIRST_NAME, CUSTOMER1_LAST_NAME, CUSTOMER1_BALANCE_IN_PENNIES, 0);
+
+    // verify that there are no logs in TransactionHistory table before Deposit
+    assertEquals(0, jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TransactionHistory;", Integer.class));
+
+    // store timestamp of when Deposit request is sent to verify timestamps in the TransactionHistory table later
+    LocalDateTime timeWhenDepositRequestSent = MvcControllerIntegTestHelpers.fetchCurrentTimeAsLocalDateTimeNoMilliseconds();
+    System.out.println("Timestamp when first Deposit Request is sent: " + timeWhenDepositRequestSent);
+
+    double[] deposits = {30.00, 40.00, 50.00, 60.00, 75.00};
+
+    for (double deposit: deposits) {
+      // Prepare Deposit Form to Deposit to customer 1's account.
+      double CUSTOMER1_AMOUNT_TO_DEPOSIT = deposit; // user input is in dollar amount, not pennies.
+      User customer1DepositFormInputs = new User();
+      customer1DepositFormInputs.setUsername(CUSTOMER1_ID);
+      customer1DepositFormInputs.setPassword(CUSTOMER1_PASSWORD);
+      customer1DepositFormInputs.setAmountToDeposit(CUSTOMER1_AMOUNT_TO_DEPOSIT); 
+
+       // send request to the Deposit Form's POST handler in MvcController
+      controller.submitDeposit(customer1DepositFormInputs);
+    }
+
+   
+
+    // fetch updated data from the DB
+    List<Map<String,Object>> customersTableData = jdbcTemplate.queryForList("SELECT * FROM Customers;");
+    List<Map<String,Object>> transactionHistoryTableData = jdbcTemplate.queryForList("SELECT * FROM TransactionHistory;");
+  
+    // verify that customer1's data is still the only data populated in Customers table
+    assertEquals(1, customersTableData.size());
+    Map<String,Object> customer1Data = customersTableData.get(0);
+    assertEquals(CUSTOMER1_ID, (String)customer1Data.get("CustomerID"));
+
+    // verify customer balance was increased by sum of deposits and applied interest rate
+    double depositSum = 0;
+    for (int i = 0; i < deposits.length; i++) {
+      depositSum += deposits[i];
+    }
+    double CUSTOMER1_EXPECTED_FINAL_BALANCE = 1.015*(CUSTOMER1_BALANCE + depositSum);
+    double CUSTOMER1_EXPECTED_FINAL_BALANCE_IN_PENNIES = MvcControllerIntegTestHelpers.convertDollarsToPennies(CUSTOMER1_EXPECTED_FINAL_BALANCE);
+    assertEquals(CUSTOMER1_EXPECTED_FINAL_BALANCE_IN_PENNIES, (int)customer1Data.get("Balance"));
+
+    // verify that there are 6 Deposits in TransactionHistory table (5 normal deposits and 1 for applied interest)
+    assertEquals(6, transactionHistoryTableData.size());
   }
   
 }
