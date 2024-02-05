ed4c236a5bd7f5591f30f83db01f921e065a4a7e
ethanzhanggg <sushisheeptwo@gmail.com>
2024-02-05 14:45:27 -0500

    Revert "Reduce logic in MvcController with the addition of a helper method"
    This reverts commit 0a6ce4c5333242451e571bece71d3a1fff15445c.

--- python-sql-scripts/credentials.py
+++ python-sql-scripts/credentials.py
@@ -1,5 +1,5 @@
 # MySQL DB Connection Config Values
 mysql_endpoint='localhost'
 username='root'
-password='Sushiisthebest1!'
+password='<Put MySQL Server Password Here>'
 database_name = 'testudo_bank'
